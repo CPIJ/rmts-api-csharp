8fd62dba81121280c98c864b5b2936a44c41ee50
Casper Pijnenburg <casper.pijnenburg@student.fontys.nl>
2017-06-05 14:10:29 +0200

    Made better return messages

--- rmts-api/Controllers/BasicController.cs
+++ rmts-api/Controllers/BasicController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Web.Http;
 using RMTS.Backend.Data.Service;
 using RMTS.Backend.Data.Service.Interface;
 
@@ -26,7 +27,7 @@ namespace RMTS.API.Controllers
         [Route("{id}")]
         public IHttpActionResult Find(int? id)
         {
-            if (id == null) return BadRequest("Geen item meegegeven");
+            if (id == null) return BadRequest("Geen geldig item meegegeven");
             if (service == null) return BadRequest("Service is not defined.");
 
             return Ok(service.Find(id.Value));
@@ -35,29 +36,58 @@ namespace RMTS.API.Controllers
         [HttpPost]
         public IHttpActionResult Create(T item)
         {
-            if (item == null) return BadRequest("Geen item meegegeven");
+            if (item == null) return BadRequest("Geen geldig item meegegeven");
             if (service == null) return BadRequest("Service is not defined.");
 
-            return Ok(service.Create(item));
+	        try
+	        {
+		        if (service.Create(item))
+			        return Ok("Item succesvol toegevoegd.");
+		        else
+					return BadRequest("Item is niet toegevoegd.");
+			}
+	        catch (Exception e)
+	        {
+		        return InternalServerError(e);
+	        }
         }
 
         [HttpPut]
         public IHttpActionResult Update(T item)
         {
-            if (item == null) return BadRequest("Geen item meegegeven");
+            if (item == null) return BadRequest("Geen geldig item meegegeven");
             if (service == null) return BadRequest("Service is not defined.");
 
-            return Ok(service.Update(item));
+	        try
+	        {
+		        if (service.Update(item))
+			        return Ok("Item succesvol geupdatet.");
+		        else
+			        return BadRequest("Item niet geupdatet.");
+	        }
+	        catch (Exception e)
+	        {
+		        return InternalServerError(e);
+	        }
         }
 
         [HttpDelete]
         [Route("{id}")]
         public IHttpActionResult Delete(int? id)
         {
-            if (id == null) return BadRequest("Geen item meegegeven");
+            if (id == null) return BadRequest("Geen geldig item meegegeven");
             if (service == null) return BadRequest("Service is not defined.");
 
-            return Ok(service.Delete(id.Value));
+	        try
+	        {
+		        if (service.Delete(id))
+			        return Ok("Item succesvol verwijderd.");
+		        return BadRequest("Item niet verwijderd.");
+	        }
+	        catch (Exception e)
+	        {
+		        return InternalServerError(e);
+	        }
         }
     }
 }
