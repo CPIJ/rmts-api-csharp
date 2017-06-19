292c13cf230ea5d70bbdafead3e29809a0499a07
Casper Pijnenburg <casper.pijnenburg@student.fontys.nl>
2017-06-19 13:42:35 +0200

    Blala

--- RMTS.Test/SortedActionsTest.cs
+++ RMTS.Test/SortedActionsTest.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RMTS.Backend.Models;
 using Action = RMTS.Backend.Models.Action;
 
